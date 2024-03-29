842b23b99a659ca911c0c7bd4f8dd0519e6de185
louis <louis.varnier@hotmail.com>
2024-03-29 16:38:44 +0100

    affichage image pokémon + stats

--- TP_final.js
+++ TP_final.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const pokemonListElement = document.getElementById("pokemon-list");
-    const pokemonDetailElement = document.getElementById("pokemon-details");
+    const pokemonDetailsElement = document.getElementById("pokemon-details");
         function afficherListePokemon() {
-        fetch("https://pokeapi.co/api/v2/pokemon/") 
+        fetch("https://pokeapi.co/api/v2/pokemon/?limit=40") 
             .then((response) => response.json())
             .then((data) => {
-            data.result.forEach((pokemon) => {
+            data.results.forEach((pokemon) => {
                 const li = document.createElement("li");
                 li.textContent = pokemon.name;
                 li.style.cursor = "pointer";
-                li.addEventListener("click", () => afficherDetailPokemon(pokemon.url));
+                li.addEventListener("click", () => afficherDetailsPokemon(pokemon.url));
                 pokemonListElement.appendChild(li);
                 });
                 })
@@ -22,6 +22,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 .then((pokemon) => {
                 pokemonDetailsElement.innerHTML = `
                 <h2>${pokemon.name}</h2>
+                <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+                <p><strong>Poids du pokémon : </strong> ${pokemon.weight}</p>
+                <p>HP : ${pokemon.stats[0].base_stat} 
+                |   Attack: ${pokemon.stats[1].base_stat} 
+                | Defense: ${pokemon.stats[2].base_stat} 
+                | SP Attack: ${pokemon.stats[3].base_stat} 
+                | SP Defense: ${pokemon.stats[4].base_stat}  
+                | Speed: ${pokemon.stats[5].base_stat}</p>
                 `;
                 })
             .catch((error) => console.error("Erreur lors de la récupération des détails des pokémons :", error));

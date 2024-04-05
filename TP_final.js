b5d5823553f4208b1580ea200875a86311d0c600
louis <louis.varnier@hotmail.com>
2024-04-05 18:41:21 +0200

    final commit

--- TP_final.js
+++ TP_final.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const pokemonListElement = document.getElementById("pokemon-list");
     const pokemonDetailsElement = document.getElementById("pokemon-details");
         function afficherListePokemon() {
-        fetch("https://pokeapi.co/api/v2/pokemon/?limit=40") 
+        fetch("https://pokeapi.co/api/v2/pokemon/?limit=1400") 
             .then((response) => response.json())
             .then((data) => {
             data.results.forEach((pokemon) => {
@@ -22,14 +22,16 @@ document.addEventListener("DOMContentLoaded", () => {
                 .then((pokemon) => {
                 pokemonDetailsElement.innerHTML = `
                 <h2>${pokemon.name}</h2>
+                <p><strong>ID du pokémon : </strong> ${pokemon.id}</p> </br>
                 <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-                <p><strong>Statistiques du pokémon </strong></p>
-                <p> HP : ${pokemon.stats[0].base_stat} 
-                | Attack: ${pokemon.stats[1].base_stat} 
-                | Defense: ${pokemon.stats[2].base_stat} 
-                | SP Attack: ${pokemon.stats[3].base_stat} 
-                | SP Defense: ${pokemon.stats[4].base_stat}  
-                | Speed: ${pokemon.stats[5].base_stat}</p>
+                <p><strong>Type du pokémon : </strong> ${pokemon.types[0].type.name} </p>
+                <p><strong>Statistiques du pokémon : </strong></p>
+                <p> HP : ${pokemon.stats[0].base_stat} </br>
+                Attack: ${pokemon.stats[1].base_stat} </br>
+                Defense: ${pokemon.stats[2].base_stat} </br>
+                SP Attack: ${pokemon.stats[3].base_stat} </br>
+                SP Defense: ${pokemon.stats[4].base_stat} </br>
+                Speed: ${pokemon.stats[5].base_stat} </br> </p>
                 `;
                 })
             .catch((error) => console.error("Erreur lors de la récupération des détails des pokémons :", error));
